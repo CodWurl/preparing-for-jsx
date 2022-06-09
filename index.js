452163efdff2fa80a160275e146220b6cf961270
azat valiev <azvaliev@gmail.com>
2022-06-09 13:46:16 -0700

    added answers

--- index.js
+++ index.js
@@ -2,33 +2,43 @@
 // (using the function keyword)
 // No parameters
 // this function should return "hello declaration"
+function greeting() {
+	return "hello declaration";
+}
 
 
 // Create a function called greeting2 using es5 function expression syntax
 // (using the function keyword)
 // No parameters
 // this function should return "hello expression"
+const greeting2 = function() {
+	return "hello expression";
+}
 
 
 // Create a function called greeting3 using es6 arrow function syntax
 // No parameters
 // this function should return "hello arrow"
-
+const greeting3 = () => {
+	return "hello arrow"
+}
 
 // Create a function called greeting4 using es6 arrow function syntax
 // (using shortened syntax - one parameter, no parentheses)
 // One parameter should be accepted and returned
+const greeting4 = param => param
 
 
 // Create a function called greeting5 using es6 arrow function syntax
 // (Standard arrow syntax, two parameters)
 // Return both parameters in an array [param1, param2]
+const greeting5 = (param1, param2) => [param1, param2];
 
 
 // Create a function called greeting6 using es6 arrow function syntax
 // (This will take two paremters, greeting and personName)
 // Change greeting to uppercase and return greeting + personName
-
+const greeting6 = (greeting, personName) => greeting.toUpperCase() + personName
 
 // Convert this to an arrow function
 const greeting7 = function() {
@@ -38,7 +48,7 @@ const greeting7 = function() {
 // Create a function called greeting8 using es6 arrow function syntax
 // (This will take two parameters, greeting and personName)
 // Use string interpolation `` to return "greeting, personName" -> replace greeting and personName with parameter values
-
+const greeting8 = (greeting, personName) => `${greeting}, ${personName}`;
 
 
 // Create a function called greeting9 using es6 arrow function syntax
@@ -50,14 +60,9 @@ const weekDays = ["monday", "tuesday", "wednesday", "thursday", "friday"];
 
 */
 // Use the .map method to return a new weekDays array, but all uppercase
-
-
-
+const greeting9 = weekDays => weekDays.map(weekDay => weekDay.toUpperCase());
 
 // Don't touch this section
-
-console.log(typeof greeting)
-
 export default {
 	greeting: typeof greeting !== 'undefined' ? greeting : () => null,
 	greeting2: typeof greeting2 !== 'undefined' ? greeting2 : () => null,
