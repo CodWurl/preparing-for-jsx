b4a5bb88690527c2e674470b171db78287175bac
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-07-13 15:28:16 -0400

    added function for greeting 2

--- tests/greeting2.test.js
+++ tests/greeting2.test.js
@@ -1,10 +1,7 @@
-import greetings from '../index';
+/*import greetings from '../index';*/
 
-const {
-	greeting2,
-} = greetings;
-
-test('greeting2() returns "hello expression"', () => {
-	const test = greeting2();
-	expect(test).toBe('hello expression');
-});
+function greeting2 () {
+	console.log('hello hello')
+  }
+  
+  greeting2('hello hello')
